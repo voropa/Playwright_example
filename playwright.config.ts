79f14e5c2c1965add7714a5f468a9c9d47048df4
Oleg Voropaev <voropaev.olegD@gmail.com>
2024-12-12 20:44:25 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,12 +46,11 @@ export default defineConfig({
       use: {
         ...devices['Desktop Chrome'],
       },
-    }
-
-    /*     {
+    },
+    {
           name: 'firefox',
           use: { ...devices['Desktop Firefox'] },
-        }, */
+    }
 
     /*         {
               name: 'webkit',
